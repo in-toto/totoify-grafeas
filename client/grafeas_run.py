067592975a31636789f0e414abdd004b51b9b497
Lukas Puehringer <lukas.puehringer@nyu.edu>
2017-12-13 15:15:17 -0500

    Misc updates in grafeas_run
     - Adds project id cli arg
     - Adds materials and products cli args
     - Fixes some occurrence typos
     - Changes in_toto_run call to not store link metadata on disk
     - General restructure

--- client/grafeas_run.py
+++ client/grafeas_run.py
@@ -1,12 +1,19 @@
 #!/usr/bin/env python
 """
-  <Name>
-    grafeas-run
+<Name>
+  grafeas_run.py
 
-  <Description>
+<Description>
+  Performs a supply chain step, gathers link metadata and submits it to the
+  server in the form of an occurrence.
 
-    performs a supply chain step, gathers link metadata and submits it to the
-    server in the form of an occurrence.
+<Usage>
+  Run git tag, generate in-toto metadata and send it to a server:
+
+  grafeas-run --id test-project --key path/to/singing/key --name tag \
+     --products . -- git tag v1.0
+
+
 
 """
 import time
@@ -20,6 +27,8 @@ import shlex
 import swagger_client
 from swagger_client.rest import ApiException
 
+from constants import SERVER_URL
+
 # in-toto specific imports
 import in_toto.runlib as runlib
 import in_toto.util as util
@@ -29,66 +38,82 @@ from pprint import pprint
 
 def parse_arguments():
   parser = argparse.ArgumentParser()
-  grafeas_run_args = parser.add_argument_group("grafeas-run options")
-  grafeas_run_args.add_argument("-t", "--target", help="the grafeas server url",
-      dest="host", default="http://localhost:8080")
-  grafeas_run_args.add_argument("-k", "--key", help="increase output verbosity")
-  grafeas_run_args.add_argument("-n", "--name", help="the name for this step")
-  grafeas_run_args.add_argument("link_cmd", nargs="*", 
+  parser.add_argument("-t", "--target", help="grafeas server url",
+      dest="target", default=SERVER_URL)
+  parser.add_argument("-i", "--id", help="project id", required=True,
+      dest="project_id")
+  parser.add_argument("-k", "--key", help="link signing key", required=True)
+  parser.add_argument("-n", "--name", help="step name", required=True)
+
+  # Record nor materials/products per default
+  parser.add_argument("-m", "--materials", help="materials to record",
+      default=[], nargs="+")
+  parser.add_argument("-p", "--products", help="products to record",
+      default=[], nargs="+")
+
+  parser.add_argument("link_cmd", nargs="*",
       help="command to be executed with options and arguments")
 
   return parser.parse_args()
-
-
-def link_to_ocurrence(name, link, project_id):
-  """
-    <Description>
-      Auxiliary method to create an occurrence out of in-toto link metadata
-  """
-  if "-" in name:
-    notename, _ = name.split("-", 1)
-  else:
-    notename = name
-
-  in_toto_link = attr.asdict(link)
-  in_toto_link['signed']['byproducts']['return-value'] = "ayylmao"
-  signable = swagger_client.LinkSignable(**(in_toto_link['signed']))
-  grafeas_link = swagger_client.LinkMetadata(signable, link.signatures)
-  this_ocurrence = swagger_client.Occurrence()
-  this_ocurrence.link_metadata = grafeas_link.to_dict()
-  this_ocurrence.name = "projects/{}/ocurrences/{}".format(project_id, name)
-  this_ocurrence.note_name = "projects/{}/notes/{}".format(project_id, notename)
-
-  return this_ocurrence 
 
 
 def main():
   args = parse_arguments()
 
   # configure our instance of the grafeas api
-  swagger_client.configuration.host = args.host
+  swagger_client.configuration.host = args.target
   api_instance = swagger_client.GrafeasApi()
 
-  # let's create an note on a test proejct here
-  projects_id = 'projects_id_example' # str | Part of `parent`. This field contains the projectId for example: \"project/{project_id}
+  project_id = args.project_id
 
   try:
-    key = False
-    if 'key' in args:
-      key = util.import_rsa_key_from_file(args.key)
-    link = runlib.in_toto_run(args.name, ['.'], ['.'], args.link_cmd, key=key)
+    # Create and return an unsigned link (not dumping to disk)
+    link = runlib.in_toto_run(args.name, args.materials, args.products,
+        args.link_cmd, key=None)
+
+    # Now sign the link with the passed key
+    key = util.import_rsa_key_from_file(args.key)
+    link.sign(key)
+
   except Exception as e:
     print("Exception when calling in-toto runlib: {}".format(e))
     sys.exit(1)
 
-  # Submit the link metadata now
-  occurrence_name = "{}-{}".format(args.name, link.signatures[0]['keyid'][:8])
-  import pdb; pdb.set_trace()
-  occurrence = link_to_ocurrence(occurrence_name, link, projects_id)
+
+  # Create an occurence from the link
+  link_dict = attr.asdict(link)
+
+  # We need to cast return-value to string or else parsing breaks mysteriously
+  link_dict["signed"]["byproducts"]["return-value"] = \
+    str(link_dict["signed"]["byproducts"]["return-value"])
+
+  # Create the in-toto link extended occurrence metadata
+  signable = swagger_client.LinkSignable(**(link_dict["signed"]))
+  grafeas_link = swagger_client.LinkMetadata(signable, link.signatures)
+
+
+  # Create the occurence object that is posted to the server
+  occurrence = swagger_client.Occurrence()
+
+  # Occurrences/links use the step name + signing keyid as id
+  # There can be multiple occurrences per note (links per step)
+  occurrence_id = "{}-{}".format(args.name, link.signatures[0]["keyid"][:8])
+  occurrence_name = "projects/{}/occurrences/{}".format(project_id,
+      occurrence_id)
+  occurrence_name = occurrence_id
+
+  # Notes/steps only use the step name as id
+  note_name = "projects/{}/notes/{}".format(project_id, args.name)
+
+  occurrence.name = occurrence_name
+  occurrence.note_name = note_name
+  occurrence.link_metadata = grafeas_link.to_dict()
+
   try:
-    api_response = api_instance.create_occurrence(projects_id, occurrence=occurrence)
-    pprint(api_response)
-    pass
+    api_response = api_instance.create_occurrence(project_id,
+        occurrence=occurrence)
+    # pprint(api_response)
+
   except ApiException as e:
     print("Exception when calling GrafeasApi->create_ocurrence: {}".format(e))
     sys.exit(1)
