bad28e6d4d6a6631d41f401bb434c4ffe6bf0d78
Lukas Puehringer <lukas.puehringer@nyu.edu>
2017-12-13 13:38:45 -0500

    Define constants used in all client scripts

---
+++ client/constants.py
@@ -0,0 +1,7 @@
+# We use different project names to namespace sets of related:
+# operation/layout - notes/steps - occurrences/links
+# Hence we can use the same LAYOUT_OCCURENCE_ID/LAYOUT_OCCURENCE_NAME in
+# each project
+LAYOUT_OCCURENCE_ID = "layout_id"
+LAYOUT_OCCURENCE_NAME = "layout_name"
+SERVER_URL = "grafeas.nyu.wtf"
